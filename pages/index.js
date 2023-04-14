b320c26a78b60b8ca4179a98065dc1fa325d672a
Armaan Gupta <armaan@guppy.im>
2023-04-14 10:45:33 -0500

    started lab code

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Here is the sentiment for your team</h1>
+    </main>
+  )
+}
