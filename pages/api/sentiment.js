98cc0c971f613a52a6782057c6dd2c79782fa1da
Armaan Gupta <armaan@guppy.im>
2023-04-28 23:56:33 -0500

    feat: returning sentiment scores for each title as well

--- pages/api/sentiment.js
+++ pages/api/sentiment.js
@@ -14,6 +14,20 @@ const getArticles = async (topic) => {
   return response.articles;
 };
 
+const getScoreMessage = (score) => {
+  if (score <= 0.1) {
+    return "Very Negative";
+  } else if (score > 0.1 && score < 0.4) {
+    return "Negative";
+  } else if (score >= 0.4 && score < 0.6) {
+    return "Neutral";
+  } else if (score >= 0.6 && score < 0.9) {
+    return "Positive";
+  } else {
+    return "Very Positive";
+  }
+};
+
 export default async function handler(req, res) {
   console.log("Sentiment analysis API started");
 
@@ -26,6 +40,7 @@ export default async function handler(req, res) {
 
   let totalScore = 0;
   let length = 0;
+  let titles = [];
 
   const articles = await getArticles(topic);
 
@@ -33,35 +48,31 @@ export default async function handler(req, res) {
     const title = articles[i].title;
     const words = title.split(" ");
     const score = analyzer.getSentiment(words);
+    const message = getScoreMessage(score);
 
-    if (score > 0) length += 1;
-
-    totalScore += score;
+    if (score > 0) {
+      length += 1;
+      totalScore += score;
+      titles.push({
+        title,
+        score,
+        message,
+      });
+    }
   }
 
   const averageScore = totalScore / articles.length;
-  let sentiment = "";
-
-  console.log(averageScore);
-
-  if (averageScore <= 0.1) {
-    sentiment = "Very Negative";
-  } else if (averageScore > 0.1 && averageScore < 0.4) {
-    sentiment = "Negative";
-  } else if (averageScore >= 0.4 && averageScore < 0.6) {
-    sentiment = "Neutral";
-  } else if (averageScore >= 0.6 && averageScore < 0.9) {
-    sentiment = "Positive";
-  } else {
-    sentiment = "Very Positive";
-  }
+  let sentiment = getScoreMessage(averageScore);
 
   res.status(200).json({
     status: 200,
     message: "Sentiment found",
     data: {
-      score: averageScore,
-      sentiment,
+      titles,
+      total: {
+        score: averageScore,
+        sentiment,
+      },
     },
   });
 }
